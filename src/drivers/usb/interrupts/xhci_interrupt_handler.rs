b889f880ac6186b89bb0b3ca9c349e816544d60a
Antoni Kuczyński <antoni.kucz@gmail.com>
2026-01-31 22:15:25 +0100

    XHCI MSI-X setup

--- src/drivers/usb/interrupts/xhci_interrupt_handler.rs
+++ src/drivers/usb/interrupts/xhci_interrupt_handler.rs
@@ -1,9 +1,14 @@
 use x86_64::structures::idt::InterruptStackFrame;
 
 pub enum XHCIInterruptIndex {
-    MsiXMessageData = 0x40
+    MsiXCommandPortData = 0x40,
+    MsiXTransferEvents = 0x41
 }
 
-pub extern "x86-interrupt" fn xhci_msix_irq_handler(_: InterruptStackFrame) {
+pub extern "x86-interrupt" fn xhci_msix_command_data_irq_handler(_: InterruptStackFrame) {
+
+}
+
+pub extern "x86-interrupt" fn xhci_msix_transfer_irq_handler(_: InterruptStackFrame) {
 
 }
